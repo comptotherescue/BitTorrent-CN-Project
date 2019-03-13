2cbdd3c267b55302b97ccaa502d844ffbc2a9584
comptotherescue <a.kulkarni110@yahoo.in>
2019-03-12 23:18:02 -0400

    [AK] Message Management Class along with BitField, Handshake and SharedFile.

--- src/Message/Message.java
+++ src/Message/Message.java
@@ -9,9 +9,6 @@ public abstract class Message {
 	private byte[] b_messageLength = new byte[4];
 	private byte[] b_payload;
 
-	public static enum Type {
-		CHOKE, UNCHOKE, INTERESTED, NOTINTERESTED, HAVE, BITFIELD, REQUEST, PIECE, HANDSHAKE;
-	}
 
 	abstract protected int getMessageLength();
 
