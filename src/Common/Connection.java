db0fa574da0849aa0f4fc0b227b31142fa7e2d27
sonal <sonal.joshi81094@gmail.com>
2019-03-28 19:19:18 -0400

    Connection handler and connection class[SJ]

--- src/Common/Connection.java
+++ src/Common/Connection.java
@@ -1,12 +1,41 @@
 package Common;
 
+import java.net.Socket;
 import java.util.BitSet;
 
 public class Connection {
 
+	public Connection(Socket socket, String peerId) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Connection(Socket socket) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public BitSet getPeerBitSet() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public int getBytesDownloaded() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void setDownloadedbytes(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean hasFile() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void getRemotePeerId() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
