6f89d9e726fdad3d020742baf60e5a3aba8d08da
comptotherescue <a.kulkarni110@yahoo.in>
2019-04-10 01:29:53 -0400

    AK - Major changes by Sonal

--- src/Common/Upload.java
+++ src/Common/Upload.java
@@ -3,6 +3,7 @@ package Common;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -13,7 +14,7 @@ public class Upload implements Runnable{
 	protected LinkedBlockingQueue<Integer> uploadLenQue;
 	protected LinkedBlockingQueue<byte[]> uploadPayQue;
 	private Socket skt;
-	private DataOutputStream outStream;
+	private ObjectOutputStream outStream;
 	private boolean isAlive;
 	// This is for Client thread initialization
 		public Upload(Socket skt, int id, SharedData data) {
@@ -30,10 +31,10 @@ public class Upload implements Runnable{
 			while (isAlive) {
 				try {
 					int msgLen = uploadLenQue.take();
-					outStream.writeInt(msgLen);
+					outStream.writeObject(new String(msgLen+""));
 					outStream.flush();
 					byte[] payload = uploadPayQue.take();
-					outStream.write(payload);
+					outStream.writeObject(payload.toString());
 					outStream.flush();
 				} catch (SocketException e) {
 					isAlive = false;
@@ -59,7 +60,7 @@ public class Upload implements Runnable{
 			isAlive = true;
 			this.skt = clientSocket;
 			try {
-				outStream = new DataOutputStream(skt.getOutputStream());
+				outStream = new ObjectOutputStream(skt.getOutputStream());
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
